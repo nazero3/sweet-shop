3cba73e05240d16c9d59488be6dc0eb735dfe730
nezirAydin <nezir.aydin94@gmail.com>
2026-05-01 19:39:22 +0300

    Updates

--- _next/static/chunks/app/order/track/[orderId]/page-cf0986185d04e34f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[419,871,88,445],{8137:function(n,e,u){Promise.resolve().then(u.t.bind(u,5411,23))}},function(n){n.O(0,[411,285,749,744],function(){return n(n.s=8137)}),_N_E=n.O()}]);
