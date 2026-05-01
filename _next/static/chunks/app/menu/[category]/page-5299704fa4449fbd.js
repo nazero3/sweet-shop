29e831f1ab65a8214cff8c4a8564c5b8cf2f3daa
nezirAydin <nezir.aydin94@gmail.com>
2026-05-01 19:37:23 +0300

    Updates

--- _next/static/chunks/app/menu/[category]/page-5299704fa4449fbd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[445,419,88,871],{8137:function(n,e,u){Promise.resolve().then(u.t.bind(u,5411,23))}},function(n){n.O(0,[411,285,749,744],function(){return n(n.s=8137)}),_N_E=n.O()}]);
--- _next/static/chunks/app/stores/page-67cf893842df7617.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[88,419,445,871],{8137:function(n,e,u){Promise.resolve().then(u.t.bind(u,5411,23))}},function(n){n.O(0,[411,285,749,744],function(){return n(n.s=8137)}),_N_E=n.O()}]);
