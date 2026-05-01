db051d5d8ffe7b54372c281790166d4162dd923b
nezirAydin <nezir.aydin94@gmail.com>
2026-05-01 19:31:14 +0300

    fix issues

--- sweet-shop/apps/web/next.config.mjs
+++ sweet-shop/apps/web/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "sweet-shop";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
